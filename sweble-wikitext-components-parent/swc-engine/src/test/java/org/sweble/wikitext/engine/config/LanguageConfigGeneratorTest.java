2d2c717d87700fb7608ce9274281d5ede5d12ae7
Richard Zowalla <rzo1@apache.org>
2025-09-18 10:19:12 +0200

    License fixes

--- sweble-wikitext-components-parent/swc-engine/src/test/java/org/sweble/wikitext/engine/config/LanguageConfigGeneratorTest.java
+++ sweble-wikitext-components-parent/swc-engine/src/test/java/org/sweble/wikitext/engine/config/LanguageConfigGeneratorTest.java
@@ -1,4 +1,6 @@
 /**
+ * Copyright 2011 The Open Source Research Group,
+ *                University of Erlangen-Nürnberg
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
